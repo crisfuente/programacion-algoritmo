2c276e57900c44b51a66a8c2819ab108b7a7cf3f
crisfuente <134553071+crisfuente@users.noreply.github.com>
2023-05-24 13:43:40 -0700

    datos solicitados

---
+++ clase 12/datos.py
@@ -0,0 +1,9 @@
+# ejemplo uso de github
+print("Ingreso de datos")
+print("---------------------\n")
+vnom=input("Ingrese su nombre:")
+vedad=input("Ingrese u edad: ")
+print("----------------------")
+print(f"Su npmbre es: {vnom} ")
+print(f"S edad es: {vedad}")
+print("Programa finalizado.")
